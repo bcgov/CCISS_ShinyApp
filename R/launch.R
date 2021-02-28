e248a4d0861ec3ff935409409395ccc26da48c5c
Bruno Tremblay <meztez@neoxone.com>
2021-02-28 14:12:23 -0500

    Moving over to cloud, laptop is too slow to create tiles

--- R/launch.R
+++ R/launch.R
@@ -5,6 +5,6 @@
 launch_app <- function() {
   rmarkdown::run(
     file = system.file("application/demo.Rmd", package = "CCISS"),
-    shiny_args = list(appDir = system.file("application", package = "CCISS"))
+    shiny_args = list()
   )
 }
