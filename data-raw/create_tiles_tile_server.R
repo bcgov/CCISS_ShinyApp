e44567fdea1bd9f8e5c9f14b741630ec52f6ce63
Bruno Tremblay <meztez@neoxone.com>
2021-02-28 14:12:23 -0500

    Moving over to cloud, laptop is too slow to create tiles

---
+++ data-raw/create_tiles_tile_server.R
@@ -0,0 +1,17 @@
+library(sf)
+library(bcdata) # Requires PR #https://github.com/bcgov/bcdata/pull/250
+library(bcmaps)
+
+# bc_bound_hres <- bc_bound_hres()
+# options("bcdata.chunk-limit" = 100)
+# bec <- bcdc_get_data(bcdc_search("BEC Map")[[1]])
+# saveRDS(bec, here::here("./data-raw/bec.RDS"))
+# bec <- readRDS(here::here("./data-raw/bec.RDS"))
+
+library(leaflet)
+library(mapboxapi)
+
+
+
+
+
