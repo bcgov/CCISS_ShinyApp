7228ed6fe49b237debc40fdec7f5b8beb1b79316
Bruno Tremblay <meztez@neoxone.com>
2021-03-22 12:44:57 -0400

    Closes #26

--- app/server/silviculture.R
+++ app/server/silviculture.R
@@ -18,3 +18,75 @@ output$silviculture_block <- renderUI({
   if (is.null(cciss_detailed)) return(NULL)
   standardblocks(siteref, siteserie, cciss_detailed)
 })
+
+# Ref template for other tables
+silv_ref_dt <- function(dt) {
+  DT::datatable(dt, rownames = FALSE, width = "100%", height = "auto", 
+                options = list(
+                  scrollCollapse = FALSE, lengthChange = FALSE, searching = FALSE,
+                  pageLength = nrow(dt), columnDefs = list(
+                    list(className = 'dt-center', targets = (seq_len(ncol(dt))-1)[-3])
+                  )
+                )
+  )
+}
+
+output$silvics_tol_dt <- DT::renderDT({
+  cciss_detailed <- uData$cciss_detailed
+  siteref <- input$siteref_silv
+  siteserie <- input$site_series_silv
+  silv_filter <- input$filter_silv
+  if (is.null(cciss_detailed)) return(NULL)
+  cciss_detailed <- cciss_detailed[SiteRef == siteref & `Site Series` %in% siteserie]
+  if (silv_filter == "f") {
+    dt <- silvics_tol[`Tree Code` %in% cciss_detailed[`Projected Feasibility` %chin% c("1", "2", "3"), Spp]]
+  } else {
+    dt <- silvics_tol
+  }
+  silv_ref_dt(dt)
+})
+
+output$silvics_resist_dt <- DT::renderDT({
+  cciss_detailed <- uData$cciss_detailed
+  siteref <- input$siteref_silv
+  siteserie <- input$site_series_silv
+  silv_filter <- input$filter_silv
+  if (is.null(cciss_detailed)) return(NULL)
+  cciss_detailed <- cciss_detailed[SiteRef == siteref & `Site Series` %in% siteserie]
+  if (silv_filter == "f") {
+    dt <- silvics_resist[`Tree Code` %in% cciss_detailed[`Projected Feasibility` %chin% c("1", "2", "3"), Spp]]
+  } else {
+    dt <- silvics_resist
+  }
+  silv_ref_dt(dt)
+})
+
+output$silvics_regen_dt <- DT::renderDT({
+  siteref <- input$siteref_silv
+  siteserie <- input$site_series_silv
+  cciss_detailed <- uData$cciss_detailed
+  silv_filter <- input$filter_silv
+  if (is.null(cciss_detailed)) return(NULL)
+  cciss_detailed <- cciss_detailed[SiteRef == siteref & `Site Series` %in% siteserie]
+  if (silv_filter == "f") {
+    dt <- silvics_regen[`Tree Code` %in% cciss_detailed[`Projected Feasibility` %chin% c("1", "2", "3"), Spp]]
+  } else {
+    dt <- silvics_regen
+  }
+  silv_ref_dt(dt)
+})
+
+output$silvics_mature_dt <- DT::renderDT({
+  cciss_detailed <- uData$cciss_detailed
+  siteref <- input$siteref_silv
+  siteserie <- input$site_series_silv
+  silv_filter <- input$filter_silv
+  if (is.null(cciss_detailed)) return(NULL)
+  cciss_detailed <- cciss_detailed[SiteRef == siteref & `Site Series` %in% siteserie]
+  if (silv_filter == "f") {
+    dt <- silvics_mature[`Tree Code` %in% cciss_detailed[`Projected Feasibility` %chin% c("1", "2", "3"), Spp]]
+  } else {
+    dt <- silvics_mature
+  }
+  silv_ref_dt(dt)
+})
