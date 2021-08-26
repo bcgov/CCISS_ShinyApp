40cdd661c65c7670e2cadbd2e38e5b0b45920912
Will MacKenzie <34073318+whmacken@users.noreply.github.com>
2021-08-26 07:21:56 -0700

    build changes

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // gs2gw
 NumericVector gs2gw(NumericVector x, double a, double b);
 RcppExport SEXP _ccissdev_gs2gw(SEXP xSEXP, SEXP aSEXP, SEXP bSEXP) {
