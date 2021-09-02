21c4e56cbf0d27efa8f6d7a1947c33e2432ec3a6
Kiri Daust <34073038+kdaust@users.noreply.github.com>
2021-09-01 23:16:12 -0700

    Fixed SS_Names

--- data-raw/scripts/bigshinygisstudio.R
+++ data-raw/scripts/bigshinygisstudio.R
@@ -117,8 +117,7 @@ analogsea::droplet_ssh(server, "R -e \"remotes::install_github('FLNRO-Smithers-R
 analogsea::droplet_upload(server, "./app/index.Rmd", "/srv/shiny-server/ccissdev/index.Rmd")
 analogsea::droplet_upload(server, "./app/www", "/srv/shiny-server/ccissdev")
 analogsea::droplet_upload(server, "./app/server", "/srv/shiny-server/ccissdev")
-analogsea::droplet_upload(server, "./app/TileOutline.gpkg", "/srv/shiny-server/ccissdev/TileOutline.gpkg")
-analogsea::droplet_upload(server, "./app/RCB_Outline.gpkg", "/srv/shiny-server/ccissdev/RCB_Outline.gpkg")
+analogsea::droplet_upload(server, "./app/NoData.gpkg", "/srv/shiny-server/ccissdev/NoData.gpkg")
 analogsea::droplet_ssh(server, "chown -R shiny:shiny /srv/shiny-server")
 analogsea::droplet_ssh(server, "systemctl restart shiny-server")
 
