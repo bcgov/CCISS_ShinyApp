e3d57a88bc7473cad584f1fdabc2df8e4df7308e
Kiri Daust <kiridaust@gmail.com>
2021-10-14 15:30:27 -0700

    Minor changes

--- Provincial_Maps.R
+++ Provincial_Maps.R
@@ -20,7 +20,6 @@ X <- raster("BC_Raster.tif")
 X <- raster::setValues(X,NA)
 outline <- st_read(con,query = "select * from bc_outline")
 ##code to check that none have the same predictions
-<<<<<<< HEAD
 allSites <- dbGetQuery(con,"select distinct rast_id from pts2km_future")
 selectSites <- sample(allSites$rast_id, size = 500, replace = F)
 dat <- dbGetQuery(con,paste0("select * from pts2km_future where rast_id IN (",
@@ -41,28 +40,7 @@ for(i in 1:(length(mods)-1)){
   }
 }
 fwrite(dat, "./GCM_BEC_agreement.csv")
-=======
-# allSites <- dbGetQuery(con,"select distinct rast_id from pts2km_future")
-# selectSites <- sample(allSites$rast_id, size = 500, replace = F)
-# dat <- dbGetQuery(con,paste0("select * from pts2km_future where rast_id IN (",
-#                              paste(selectSites,collapse = ","),") and futureperiod = '2041-2060' and scenario = 'ssp245'"))
-# setDT(dat)
-# dat <- dcast(dat,rast_id ~ gcm,value.var = "bgc_pred", fun.aggregate = function(x)x[1])
-# mods <- names(dat)[-1]
-# dat[,rast_id := NULL]
-# 
-# for(i in 1:(length(mods)-1)){
-#   for(j in (i+1):length(mods)){
-#     if(all(dat[,..i] == dat[,..j])){
-#       cat("Predictions", mods[i],"and",mods[j], "are identical!")
-#     }
-#     cat("Models:",mods[i],mods[j],"\n")
-#     temp <- dat[,..i] == dat[,..j]
-#     print(table(temp))
-#   }
-# }
 
->>>>>>> 29ade9628250f77c21dfafec39e02ce244402472
 ##########################################################
 
 ##make projected bgc maps
