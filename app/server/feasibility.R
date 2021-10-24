075e5a1c6ba6cd472a311f12498e51ef73098b29
Kiri Daust <34073038+kdaust@users.noreply.github.com>
2021-10-24 13:52:48 -0700

    Various updates

--- app/server/feasibility.R
+++ app/server/feasibility.R
@@ -16,6 +16,31 @@ output$results_feas <- function() {
   cciss_results_dt(cciss_results, siteref, siteserie, feas_filter)
 }
 
+output$edaplot <- renderPlot({
+  SS <- input$site_series_feas
+  eda <- unique(E1[SS_NoSpace == SS,Edatopic])
+  if(length(eda) >= 1){
+    edaGrid[,Col := "grey"]
+    edaGrid[edatopic %in% eda, Col := "red"]
+    ggplot()+
+      geom_blank()+
+      scale_y_discrete(limits = c("7","6","5","4","3","2","1","0"))+
+      scale_x_discrete(limits = c("A","B","C","D","E"))+
+      geom_raster(data = edaGrid, aes(x = X,y = Y, fill = Col))+
+      geom_hline(aes(yintercept = grd1y), linetype = "dashed")+
+      geom_vline(aes(xintercept = grd1x), linetype = "dashed")+
+      theme_bw(base_size = 16)+
+      theme(panel.grid.major = element_blank())+
+      labs(x = "SNR", y = "SMR")+
+      theme(plot.background = element_rect(fill = "transparent", colour = NA),
+            legend.position = "none")+
+      coord_fixed()
+  }
+},bg="transparent")
+
+grd1x <- seq(1.5,4.5,1)
+grd1y <- seq(1.5,7.5,1)
+
 # Dual utility function to format dt, app mode and report mode use different
 # format. Report has no javascript, just a plain table.
 cciss_results_dt <- function(data, siteref, siteserie, filter, format = "html") {
@@ -42,6 +67,10 @@ cciss_results_dt <- function(data, siteref, siteserie, filter, format = "html") 
   data[,Trend := gsub("PUPPY","<br />",Trend)]
   data[,Trend := gsub("SU","<u>",Trend)]
   data[,Trend := gsub("EU","</u>",Trend)]
+  
+  data[,Period := gsub("<br />","BRK",Period)]
+  data[,Period := cell_spec(Period,font_size = 12)]
+  data[,Period := gsub("BRK","<br />",Period)]
 
   data <- data[SiteRef == siteref & SS_NoSpace %in% siteserie,
                .(Species, Period, PredFeasSVG, CFSuitability, PrefAcc_Orig, PrefAcc, 
@@ -72,7 +101,7 @@ cciss_results_dt <- function(data, siteref, siteserie, filter, format = "html") 
                          "CCISS" = 4, "Trend" = 1)) %>%
       column_spec(4:10,bold = T, extra_css = "vertical-align:middle;") %>%
       column_spec(1, tooltip = hoverText[1]) %>%
-      column_spec(2, tooltip = hoverText[2]) %>%
+      column_spec(2, tooltip = hoverText[2], width = "3cm") %>%
       column_spec(3, tooltip = hoverText[3]) %>%
       column_spec(4, tooltip = hoverText[4]) %>%
       column_spec(5, tooltip = hoverText[5]) %>%
