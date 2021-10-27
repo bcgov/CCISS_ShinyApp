91a4c742043965ca12c41333e93a25e4bb818ece
Bruno Tremblay <meztez@neoxone.com>
2021-10-27 02:40:00 -0400

    Exploit array to avoid byte padding

--- data-raw/scripts/cciss_future_table_downsizing.R
+++ data-raw/scripts/cciss_future_table_downsizing.R
@@ -1,5 +1,7 @@
 library(RPostgres)
 library(DBI)
+library(data.table)
+
 conn <- DBI::dbConnect(
   drv = RPostgres::Postgres(),
   dbname = Sys.getenv("BCGOV_DB"),
@@ -47,11 +49,104 @@ query <- "
 
 dbExecute(conn, query)
 
+# It seems everything has to take at least 8 bytes to respect columns alignment
+dbExecute(conn, "DROP TABLE cciss_future12smlr_test")
+
 query <- "
-  CREATE TABLE cciss_future12SMLR (
-    siteno INTEGER REFERENCES hex_points
-    futureperiod 
+  CREATE TABLE cciss_future12smlr_test (
+    siteno INTEGER REFERENCES hex_points,
+    gcm_id SMALLINT REFERENCES gcm,
+    scenario_id SMALLINT REFERENCES scenario,
+    futureperiod_id SMALLINT REFERENCES futureperiod,
+    bgc_pred_id SMALLINT REFERENCES bgc
   )
 "
 
 dbExecute(conn, query)
+
+query <- "
+EXPLAIN (ANALYSE, BUFFERS)
+INSERT INTO cciss_future12smlr_test (
+  siteno,
+  gcm_id,
+  scenario_id,
+  futureperiod_id,
+  bgc_pred_id
+)
+SELECT
+  rast_id siteno,
+  gcm.gcm_id,
+  scenario.scenario_id,
+  futureperiod.futureperiod_id,
+  bgc.bgc_id bgc_pred_id
+FROM (SELECT * FROM pts2km_future WHERE rast_id between 241002 and 250000) pts2km_future
+JOIN gcm ON (pts2km_future.gcm = gcm.gcm)
+JOIN scenario ON (pts2km_future.scenario = scenario.scenario)
+JOIN futureperiod ON (pts2km_future.futureperiod = futureperiod.futureperiod)
+JOIN bgc ON (pts2km_future.bgc_pred = bgc.bgc);
+"
+
+(res <- dbGetQuery(conn, query))
+dbGetQuery(conn, "SELECT pg_table_size('cciss_future12smlr_test')")
+dt_test<-dbReadTable(conn, "cciss_future12smlr_test")
+setDT(dt_test)
+dt_test <- dt_test[order(gcm_id, scenario_id, futureperiod_id)]
+# Making we are not skipping values
+unique(dt_test$gcm_id)
+unique(dt_test$scenario_id)
+unique(dt_test$futureperiod_id)
+
+
+# Handle missing combinations
+idx_ipt_len <- function(index, input, length) {
+  x <- NA_integer_
+  length(x) <- length
+  x[index] <- input
+  x
+}
+
+insert <- dt_test[,
+  list(bgc_pred_id = paste0(idx_ipt_len(futureperiod_id, bgc_pred_id, 5L), collapse = ",")),
+  by = list(siteno, gcm_id, scenario_id)
+][,
+  list(bgc_pred_id = paste0("{", idx_ipt_len(scenario_id, bgc_pred_id, 4L), "}", collapse = ",")),
+  by = list(siteno, gcm_id)
+][,
+  list(bgc_pred_id = paste0("'{", paste0("{", idx_ipt_len(gcm_id, bgc_pred_id, 13L), "}", collapse = ","), "}'")),
+  by = list(siteno)
+][,
+  list(siteno, bgc_pred_id = gsub("NA", "NULL", bgc_pred_id, fixed = TRUE))
+]
+
+dbExecute(conn, "DROP TABLE cciss_future12smlr_test2")
+
+query <- "
+  CREATE TABLE cciss_future12smlr_test2 (
+    siteno INTEGER REFERENCES hex_points,
+    -- [gcm][scenario][futureperiod]
+    bgc_pred_id SMALLINT[13][4][5]
+  )
+"
+dbExecute(conn, query)
+
+query <- paste0("
+EXPLAIN (ANALYSE, BUFFERS)
+INSERT INTO cciss_future12smlr_test2 (
+  siteno,
+  bgc_pred_id
+) VALUES ",
+paste0("(", insert$siteno, ", ", insert$bgc_pred_id, ")", collapse = ", "))
+
+
+dbExecute(conn, query)
+
+# Writing queries and updating table will get slightly more complex but the size saving are huge
+# and the performance are back
+dbGetQuery(conn, "SELECT pg_table_size('cciss_future12smlr_test2')") /
+dbGetQuery(conn, "SELECT pg_table_size('cciss_future12smlr_test')")
+
+# [gcm][scenario][futureperiod]
+dbGetQuery(conn, "SELECT siteno, bgc_pred_id[2][1][2] FROM cciss_future12smlr_test2")
+dbGetQuery(conn, "SELECT siteno, bgc_pred_id[2:8][1][2] FROM cciss_future12smlr_test2")
+dbGetQuery(conn, "SELECT siteno, unnest(bgc_pred_id[1:4][1:4][2]) FROM cciss_future12smlr_test2")
+
