ff95a3d7714f81e041478ebe1e0213f6fed3f2f6
Bruno Tremblay <meztez@neoxone.com>
2021-10-31 14:03:49 -0400

    Add an help icon link next to section title.

--- app/server/instructions.R
+++ app/server/instructions.R
@@ -27,3 +27,35 @@ observeEvent(input$cciss_instructions_export, {
   updateNavbarPage(inputId = "cciss_navbar", selected = "cciss_instructions")
   updateTabsetPanel(inputId = "cciss_instructions_set", selected = "cciss_instructions_export")
 })
+
+# Nav
+
+observeEvent(input$cciss_instructions_select_sites_nav, {
+  updateNavbarPage(inputId = "cciss_navbar", selected = "cciss_instructions")
+  updateTabsetPanel(inputId = "cciss_instructions_set", selected = "cciss_instructions_select_sites")
+})
+
+observeEvent(input$cciss_instructions_feasibility_report_nav, {
+  updateNavbarPage(inputId = "cciss_navbar", selected = "cciss_instructions")
+  updateTabsetPanel(inputId = "cciss_instructions_set", selected = "cciss_instructions_feasibility_report")
+})
+
+observeEvent(input$cciss_instructions_bec_futures_nav, {
+  updateNavbarPage(inputId = "cciss_navbar", selected = "cciss_instructions")
+  updateTabsetPanel(inputId = "cciss_instructions_set", selected = "cciss_instructions_bec_futures")
+})
+
+observeEvent(input$cciss_instructions_silvics_ecology_nav, {
+  updateNavbarPage(inputId = "cciss_navbar", selected = "cciss_instructions")
+  updateTabsetPanel(inputId = "cciss_instructions_set", selected = "cciss_instructions_silvics_ecology")
+})
+
+observeEvent(input$cciss_instructions_species_portfolio_nav, {
+  updateNavbarPage(inputId = "cciss_navbar", selected = "cciss_instructions")
+  updateTabsetPanel(inputId = "cciss_instructions_set", selected = "cciss_instructions_species_portfolio")
+})
+
+observeEvent(input$cciss_instructions_export_nav, {
+  updateNavbarPage(inputId = "cciss_navbar", selected = "cciss_instructions")
+  updateTabsetPanel(inputId = "cciss_instructions_set", selected = "cciss_instructions_export")
+})
