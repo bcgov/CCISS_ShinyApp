76ca2d02a381b39cc5951eeb3ad04acc55ede3b0
Will MacKenzie <34073318+whmacken@users.noreply.github.com>
2021-11-13 16:07:34 -0800

    moved scripts around

--- CCISS_Spp_Map_Graphics2.R
+++ CCISS_Spp_Map_Graphics2.R
@@ -26,7 +26,7 @@ con <- dbConnect(drv, user = "postgres",
 X <- raster("BC_Raster.tif")
 X <- raster::setValues(X,NA)
 outline <- st_read(con,query = "select * from bc_outline")
-bgc <- dbGetCCISS_4km(con,timeperiods,all_weight) ##takes about 1.5 mins
+
 
 ##adjust gcm weight or rcp weight below
 gcm_weight <- data.table(gcm = c("ACCESS-ESM1-5", "BCC-CSM2-MR", "CanESM5", "CNRM-ESM2-1", "EC-Earth3", 
@@ -44,37 +44,6 @@ all_weight[,weight := wgcm*wrcp]
 modWeights <- all_weight
 
 
-####Range expansion and retreat maps
-breakpoints <- seq(-1,1,0.2); length(breakpoints)
-labels <- c("Retreat", "Expansion")
-ColScheme <- c(brewer.pal(11,"RdBu")[c(1:4)], "grey70", brewer.pal(11,"RdBu")[c(7:11)]); length(ColScheme)
-
-
-for(spp in c("Cw","Yc", "Oa", "Yp")){ ##ignore warnings"Fd","Sx","Pl",
-  cat("Plotting ",spp,"\n")
-  addret <- add_retreat_map(SSPreds,S1,spp) ##~ 15 seconds
-  addret[Flag == "Same",PropMod := 0]
-  addret <- addret[addret[, .I[which.max(abs(PropMod))], by= .(SiteRef)]$V1]
-  X <- raster::setValues(X,NA)
-  X[addret$SiteRef] <- addret$PropMod
-  png(file=paste("./FeasibilityMaps/Add_Retreat",timeperiods,spp,".png",sep = "_"), type="cairo", units="in", width=6.5, height=7, pointsize=10, res=800)
-  #pdf(file=paste("./FeasibilityMaps/Add_Retreat",timeperiods,spp,".pdf",sep = "_"), width=6.5, height=7, pointsize=10)
-  image(X,xlab = NA,ylab = NA,bty = "n",  xaxt="n", yaxt="n", 
-        col=ColScheme, breaks=breakpoints, maxpixels= ncell(X),
-        main = paste0(T1[TreeCode == spp,EnglishName]," (",spp,")\nSite Type: ",edaPos))
-  plot(outline, add=T, border="black",col = NA, lwd=0.4)
-  
-  par(xpd=T)
-  
-  #xl <- 325000; yb <- 900000; xr <- 425000; yt <- 1525000
-  xl <- 1600000; yb <- 1000000; xr <- 1700000; yt <- 1700000
-  rect(xl,  head(seq(yb,yt,(yt-yb)/length(ColScheme)),-1),  xr,  tail(seq(yb,yt,(yt-yb)/length(ColScheme)),-1),  col=ColScheme)
-  text(rep(xr+10000,length(labels)),seq(yb,yt,(yt-yb)/(15-1))[c(3,9)],labels,pos=4,cex=0.9,font=0.8, srt=90)
-  text(rep(xr-20000,length(labels)),seq(yb,yt,(yt-yb)/(15-1))[c(1,8,15)],c("100%", "0%", "100%"),pos=4,cex=0.8,font=1)
-  text(xl-30000, mean(c(yb,yt))-30000, paste("Change to feasible/unfeasible\n(", timeperiods, ") % of GCMs", sep=""), srt=90, pos=3, cex=0.9, font=2)
-  dev.off()
-}
-
 
 ##figure 3c (mean change in feasibility by edatopic position)
 library(RColorBrewer)
@@ -83,8 +52,10 @@ labels <- c("-3","-2", "-1", "no change", "+1","+2","+3")
 ColScheme <- c(brewer.pal(11,"RdBu")[c(1,2,3,4,4)], "grey50", brewer.pal(11,"RdBu")[c(7,8,8,9,10,11)]); length(ColScheme)
 
 timeperiods <- "2041-2060"
-edaPos <- "B2"
+edaPos <- "C4"
 edaZonal <- E1[Edatopic == edaPos,]
+
+bgc <- dbGetCCISS_4km(con,timeperiods,all_weight) ##takes about 1.5 mins
 ##edatopic overlap
 SSPreds <- edatopicOverlap(bgc,edaZonal,E1_Phase) ##takes about 30 seconds
 #SSPreds <- SSPreds[grep("01$|h$|00$",SS_NoSpace),] ##note that all below plots are reusing this SSPreds data
@@ -116,6 +87,37 @@ for(spp in c("Cw","Yc", "Oa", "Yp")){ ##ignore warnings"Fd","Sx","Pl", ,"Oa", "Y
   dev.off()
 }
 
+####Range expansion and retreat maps (continure from run above)
+breakpoints <- seq(-1,1,0.2); length(breakpoints)
+labels <- c("Retreat", "Expansion")
+ColScheme <- c(brewer.pal(11,"RdBu")[c(1:4)], "grey50", brewer.pal(11,"RdBu")[c(7:11)]); length(ColScheme)
+
+
+for(spp in c("Cw","Yc", "Oa", "Yp")){ ##ignore warnings"Fd","Sx","Pl",
+  cat("Plotting ",spp,"\n")
+  addret <- add_retreat_map(SSPreds,S1,spp) ##~ 15 seconds
+  addret[Flag == "Same",PropMod := 0]
+  addret <- addret[addret[, .I[which.max(abs(PropMod))], by= .(SiteRef)]$V1]
+  X <- raster::setValues(X,NA)
+  X[addret$SiteRef] <- addret$PropMod
+  png(file=paste("./FeasibilityMaps/Add_Retreat",timeperiods,spp,edaPos,".png",sep = "_"), type="cairo", units="in", width=6.5, height=7, pointsize=10, res=800)
+  #pdf(file=paste("./FeasibilityMaps/Add_Retreat",timeperiods,spp,".pdf",sep = "_"), width=6.5, height=7, pointsize=10)
+  image(X,xlab = NA,ylab = NA,bty = "n",  xaxt="n", yaxt="n", 
+        col=ColScheme, breaks=breakpoints, maxpixels= ncell(X),
+        main = paste0(T1[TreeCode == spp,EnglishName]," (",spp,")\nSite Type:" ,edaPos, "\nTime Period:", timeperiods))
+  plot(outline, add=T, border="black",col = NA, lwd=0.4)
+  
+  par(xpd=T)
+  
+  #xl <- 325000; yb <- 900000; xr <- 425000; yt <- 1525000
+  xl <- 1600000; yb <- 1000000; xr <- 1700000; yt <- 1700000
+  rect(xl,  head(seq(yb,yt,(yt-yb)/length(ColScheme)),-1),  xr,  tail(seq(yb,yt,(yt-yb)/length(ColScheme)),-1),  col=ColScheme)
+  text(rep(xr+10000,length(labels)),seq(yb,yt,(yt-yb)/(15-1))[c(3,9)],labels,pos=4,cex=0.9,font=0.8, srt=90)
+  text(rep(xr-20000,length(labels)),seq(yb,yt,(yt-yb)/(15-1))[c(1,8,15)],c("100%", "0%", "100%"),pos=4,cex=0.8,font=1)
+  text(xl-30000, mean(c(yb,yt))-30000, paste("Change to feasible/unfeasible\n(", timeperiods, ") % of GCMs", sep=""), srt=90, pos=3, cex=0.9, font=2)
+  dev.off()
+}
+
 
 
 ##show driest edatopic maps
@@ -126,7 +128,7 @@ feas_cutoff <- 3
 
 feas_cutoff <- feas_cutoff+0.5
 
-#bgc <- dbGetCCISS_4km(con,timeperiods,all_weight) ##takes about 1.5 mins
+bgc <- dbGetCCISS_4km(con,timeperiods,all_weight) ##takes about 1.5 mins
 edaBlobs <- fread("EdaBlobs.csv")
 
 
@@ -186,13 +188,13 @@ bgcMap <- bgcMap[!is.na(Col),]
 bgcMap <- st_as_sf(bgcMap)
 
 
-# png(file=paste("./FeasibilityMaps/EdaByBGC_Current",spp,".png",sep = "_"), type="cairo", units="in", width=6.5, height=7, pointsize=10, res=800)#
-# plot(bgcMap["BGC"],col = bgcMap$Col,lty = 0,main = paste0("Edatopic Feasibility for ",spp," (Current)"))
-# plot(outline, col = NA, lwd=0.4, add = T)
-# legend(x = "bottomleft",
-#        legend = labels,
-#        fill = ColScheme,
-#        title = "Driest Feasible rSMR")
+png(file=paste("./FeasibilityMaps/EdaByBGC_Current",spp,".png",sep = "_"), type="cairo", units="in", width=6.5, height=7, pointsize=10, res=800)#
+plot(bgcMap["BGC"],col = bgcMap$Col,lty = 0,main = paste0("Edatopic Feasibility for ",spp," (Current)"))
+plot(outline, col = NA, lwd=0.4, add = T)
+legend(x = "bottomleft",
+       legend = labels,
+       fill = ColScheme,
+       title = "Driest Feasible rSMR")
 
 png(file=paste("./FeasibilityMaps/EdaByBGC_Current",spp,".png",sep = "_"), type="cairo", units="in", width=6.5, height=7, pointsize=10, res=800)
 ggplot(bgcMap) +
